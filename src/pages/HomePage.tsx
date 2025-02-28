ca01bee2242a3a07185ffa3b3b2eca366be0a77b
BS <bohdan.strilets@gmail.com>
2025-02-28 21:23:21 +0100

    Added routing.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react'
+
+const HomePage: FC = () => {
+	return (
+		<>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem facilis
+				commodi ipsum dolore ea odit, libero mollitia perferendis tempore iusto,
+				exercitationem possimus in et illo officiis magnam id. Consequatur,
+				ullam.
+			</p>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam atque
+				corporis aliquam magnam. Incidunt porro sapiente praesentium quod culpa,
+				fuga eligendi ipsum autem consequuntur labore. Recusandae exercitationem
+				deserunt nam nemo!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ullam
+				illo molestias praesentium consequuntur! Quae, nostrum. Magnam, illum
+				velit pariatur dolorum incidunt amet consequuntur aperiam, rem enim
+				provident est deserunt excepturi perspiciatis quae quos exercitationem
+				saepe sapiente optio culpa iusto necessitatibus maiores. Totam
+				reprehenderit sapiente eos maxime magni rem eaque est soluta asperiores
+				architecto, animi dolor, vel veritatis quod fugit perferendis. Soluta
+				voluptatum itaque molestias placeat, nihil et! Quos voluptatum sint
+				animi quam, ducimus sunt consequuntur nulla voluptatem nostrum
+				consequatur eum ratione blanditiis culpa fuga, ut corporis rerum neque
+				eveniet fugiat! Quos ad, provident explicabo aperiam a ea, quis nam
+				nostrum sint eaque quasi ut ipsa placeat voluptatum aliquid optio
+				consequuntur architecto natus minus quia? Provident possimus sit
+				exercitationem enim suscipit dolores eaque. Quo possimus, laborum neque
+				pariatur quam error totam perferendis facilis tempora ab atque quidem
+				explicabo sit, tempore cupiditate natus dolores ipsum similique ex?
+				Inventore, necessitatibus! Beatae modi praesentium fugiat cumque a ipsum
+				doloribus aut laboriosam optio quia voluptate consequuntur debitis,
+				delectus laudantium eos provident vel quasi rerum itaque fugit libero
+				explicabo! Iusto minima totam eos deleniti, quas dolor? Illum doloribus
+				pariatur voluptatum excepturi. Vero, porro quia quasi qui, iste fugiat
+				doloremque praesentium nesciunt iusto recusandae quisquam accusamus!
+			</p>
+		</>
+	)
+}
+
+export default HomePage
