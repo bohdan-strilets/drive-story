7501c06f8cd1263efd07f133afb0bef73b9457f5
BS <bohdan.strilets@gmail.com>
2025-03-12 16:25:16 +0100

    Install lib for management state.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
 import Layout from './components/Layout'
+import { queryClient } from './config/queryClient.ts'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Layout>
-				<App />
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
 			</Layout>
 		</BrowserRouter>
 	</StrictMode>
