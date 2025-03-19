9120f07a542724df7e73449de152852a71256f39
BS <bohdan.strilets@gmail.com>
2025-03-19 09:47:52 +0100

    Added an interceptor that adds an authorization token when sending a request.

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,10 +1,30 @@
 import axios from 'axios'
 
+import { authStorageKey } from './localStorageKeys'
+
 export const API_URL = 'http://localhost:4000/api/v1/'
 
 const apiClient = axios.create({
 	withCredentials: true,
 	baseURL: API_URL,
+})
+
+apiClient.interceptors.request.use((config) => {
+	const dataFromLocalStorage = localStorage.getItem(authStorageKey)
+	let parsedData = null
+
+	if (dataFromLocalStorage) {
+		try {
+			parsedData = JSON.parse(dataFromLocalStorage)
+		} catch (error) {
+			console.error('Error parsing data from localStorage:', error)
+		}
+	}
+
+	const accessToken: string | null = parsedData?.state?.token
+	if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`
+
+	return config
 })
 
 export default apiClient
