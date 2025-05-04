40a581bec5b92f229a787d6601c8a14c8c80bb05
BS <bohdan.strilets@gmail.com>
2025-05-04 17:40:43 +0200

    Fixed the position of the button on top of the other elements.

--- src/components/UI/ButtonToTop/ButtonToTop.styled.ts
+++ src/components/UI/ButtonToTop/ButtonToTop.styled.ts
@@ -4,6 +4,7 @@ export const Wrapper = styled.div<{ visible: boolean }>`
 	position: fixed;
 	bottom: 15px;
 	right: 15px;
+	z-index: 999;
 
 	opacity: ${({ visible }) => (visible ? 1 : 0)};
 	transform: translateY(${({ visible }) => (visible ? '0' : '20px')});
