6aa2afd343da0e042759bc84c9fdd551cd095382
BS <bohdan.strilets@gmail.com>
2025-06-04 18:28:11 +0200

    Replaced the image to show the empty state.

--- src/components/Insurance/NoInsuranceState/NoInsuranceState.tsx
+++ src/components/Insurance/NoInsuranceState/NoInsuranceState.tsx
@@ -2,14 +2,20 @@ import { FC } from 'react'
 import { IoDocumentText } from 'react-icons/io5'
 
 import ActionButton from '@/components/UI/ActionButton'
-import EmptyState from '@/components/UI/EmptyState'
+import ImageBox from '@/components/UI/ImageBox'
+import Paragraph from '@/components/UI/Paragraph'
+import Title from '@/components/UI/Title'
 
 import useModal from '@/hooks/ui/useModal'
+import useResponsive from '@/hooks/ui/useResponsive'
+
+import noInsuranceImage from '@/assets/emptyList/no-insurance.webp'
 
 import { modalNames } from '@/config/modalConfig'
 
 const NoInsuranceState: FC = () => {
 	const { onOpen } = useModal()
+	const { maxTablet } = useResponsive()
 
 	return (
 		<>
@@ -23,10 +29,22 @@ const NoInsuranceState: FC = () => {
 				margin="0 0 30px 0"
 			/>
 
-			<EmptyState
-				title="No Insurance Policy Yet"
-				message="You have not created an insurance policy for this vehicle yet. If you’d like to create one, please click the “Add Insurance” button above."
-			/>
+			<>
+				<Title fontSize={22} textAlign="center" color="yellow">
+					No Insurance Policy Yet
+				</Title>
+				<ImageBox
+					imageUrl={noInsuranceImage}
+					width="100%"
+					height={maxTablet ? '300px' : '500px'}
+					size="contain"
+				/>
+				<Paragraph color="black" fontWeight={600} textAlign="center">
+					You have not created an insurance policy for this vehicle yet. If
+					you’d like to create one, please click the “Add Insurance” button
+					above.
+				</Paragraph>
+			</>
 		</>
 	)
 }
