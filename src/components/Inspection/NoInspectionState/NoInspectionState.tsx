17fd1ec10128bad9a73f76abf59c64fd93dab826
BS <bohdan.strilets@gmail.com>
2025-06-04 18:13:19 +0200

    Replaced the image to show a blank insurance policy page.

--- src/components/Inspection/NoInspectionState/NoInspectionState.tsx
+++ src/components/Inspection/NoInspectionState/NoInspectionState.tsx
@@ -2,14 +2,20 @@ import { FC } from 'react'
 import { MdMiscellaneousServices } from 'react-icons/md'
 
 import ActionButton from '@/components/UI/ActionButton'
-import EmptyState from '@/components/UI/EmptyState'
+import ImageBox from '@/components/UI/ImageBox'
+import Paragraph from '@/components/UI/Paragraph'
+import Title from '@/components/UI/Title'
 
 import useModal from '@/hooks/ui/useModal'
+import useResponsive from '@/hooks/ui/useResponsive'
+
+import noInspectionImage from '@/assets/emptyList/no-inspection.webp'
 
 import { modalNames } from '@/config/modalConfig'
 
 const NoInspectionState: FC = () => {
 	const { onOpen } = useModal()
+	const { maxTablet } = useResponsive()
 
 	return (
 		<>
@@ -23,10 +29,21 @@ const NoInspectionState: FC = () => {
 				margin="0 0 30px 0"
 			/>
 
-			<EmptyState
-				title="No information about technical inspection Yet"
-				message="You have not yet added information about the technical inspection of your car. You can do this using the button above."
-			/>
+			<>
+				<Title fontSize={22} textAlign="center" color="yellow">
+					No information about technical inspection Yet
+				</Title>
+				<ImageBox
+					imageUrl={noInspectionImage}
+					width="100%"
+					height={maxTablet ? '300px' : '500px'}
+					size="contain"
+				/>
+				<Paragraph color="black" fontWeight={600} textAlign="center">
+					You have not yet added information about the technical inspection of
+					your car. You can do this using the button above.
+				</Paragraph>
+			</>
 		</>
 	)
 }
